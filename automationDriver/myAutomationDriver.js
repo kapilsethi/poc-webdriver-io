ef2017fc74d8a1cf592bb6467f956cf5a6e7bdec
Kapil Sethi <kapil.sethi9@gmail.com>
2019-09-03 23:58:48 +1000

    adding tests to check start over button functionality (#4)

--- automationDriver/myAutomationDriver.js
+++ automationDriver/myAutomationDriver.js
@@ -1,30 +1,42 @@
 class MyAutomationDriver {
     openPage(pageUrl) {
         browser.url(pageUrl);
-    }
+    };
 
     getPageTitle() {
         return browser.getTitle();
-    }
+    };
 
     click(identifier) {
         $(identifier).click();
-    }
+    };
 
     enterText(identifier, textToEnter) {
         $(identifier).setValue(textToEnter);
-    }
+    };
 
     selectOption(identifier, optionToSelect) {
         $(identifier).selectByVisibleText(optionToSelect);
-    }
+    };
 
     getText(identifier) {
         return $(identifier).getText();
-    }
+    };
 
     waitForDisplayed(identifier) {
         $(identifier).waitForDisplayed();
+    };
+
+    isDisplayed(identifier) {
+        return $(identifier).isDisplayed();
+    };
+
+    scrollIntoView(identifier) {
+        $(identifier).scrollIntoView();
+    };
+
+    getValue(identifier) {
+        return $(identifier).getValue();
     }
-}
+};
 module.exports = MyAutomationDriver;
