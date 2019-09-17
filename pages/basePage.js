2096d7e96742dc154473ab018d566549bab3694f
Kapil Sethi <kapil.sethi9@gmail.com>
2019-09-18 09:37:32 +1000

    using eslint and fixing errors

--- pages/basePage.js
+++ pages/basePage.js
@@ -4,19 +4,19 @@ class BasePage {
     constructor() {
         if(this.myDriver == null) {
             this.myDriver = new MyAutomationDriver();
-        };
-    };
+        }
+    }
 
     getMyDriver() {
         return this.myDriver;
-    };
+    }
     
     open(pageUrl) {
         this.myDriver.openPage(pageUrl);
-    };
+    }
 
     getPageTitle() {
         return this.myDriver.getPageTitle();
-    };
-};
+    }
+}
 module.exports = BasePage;
--- pages/howMuchCouldIBorrow.js
+++ pages/howMuchCouldIBorrow.js
@@ -19,89 +19,89 @@ class HowMuchCouldIBorrow extends BasePage {
         this.howMuchCouldIBorrowTextResult = ".borrow__result__text[aria-live='assertive']";
         this.howMuchCouldIBorrowAmountResult = ".borrow__result__text__amount";
         this.startOver = ".borrow__result .icon_restart";
-    };
+    }
 
     selectApplicationTypeAsSingle() {
         this.driver.click(this.singleApplicationType);
-    };
+    }
 
     selectNumberOfDependents(numberOfDependantsToSelect) {
         this.driver.selectOption(this.numberOfDependants, numberOfDependantsToSelect);
-    };
+    }
 
     selectHomeToLiveIn() {
         this.driver.click(this.homeToLiveIn);
-    };
+    }
 
     enterIncomeBeforeTax(incomeToEnter) {
         this.driver.enterText(this.incomeBeforeTax, incomeToEnter);
-    };
+    }
 
     enterOtherIncome(otherIncomeToEnter) {
         this.driver.enterText(this.otherIncome, otherIncomeToEnter);
-    };
+    }
 
     enterLivingExpenses(livingExpensesToEnter) {
         this.driver.enterText(this.livingExpenses, livingExpensesToEnter);
-    };
+    }
 
     enterCurrentHomeLoanRepayments (currentHomeLoanRepaymentsToEnter) {
         this.driver.enterText(this.currentHomeLoanRepayments, currentHomeLoanRepaymentsToEnter)
-    };
+    }
 
     enterOtherLoanRepayments(otherLoanRepaymentsToEnter) {
         this.driver.enterText(this.otherLoanRepayments, otherLoanRepaymentsToEnter)
-    };
+    }
 
     enterOtherCommitments(otherCommitmentsToEnter) {
         this.driver.enterText(this.otherCommitments, otherCommitmentsToEnter);
-    };
+    }
 
     enterTotalCreditCardLimits(totalCreditCardLimitsToEnter) {
         this.driver.enterText(this.totalCreditCardLimits, totalCreditCardLimitsToEnter)
-    };
+    }
 
     clickHowMuchCanIBorrowButton() {
         this.driver.scrollIntoView(this.howMuchCouldIBorrowButton);
         this.driver.click(this.howMuchCouldIBorrowButton);
-    };
+    }
 
     getHowMuchCanIBorrowTextResult() {
         this.driver.waitForDisplayed(this.howMuchCouldIBorrowTextResult);
         return this.driver.getText(this.howMuchCouldIBorrowTextResult);
-    };
+    }
 
     getHowMuchCanIBorrowAmountResult() {
         return this.driver.getText( this.howMuchCouldIBorrowAmountResult);
-    };
+    }
 
     clickStartOver() {
         this.driver.click(this.startOver);
-    };
+    }
 
     isHowMuchCouldIBorrowDisplayed() {
         return this.driver.isDisplayed(this.howMuchCouldIBorrowButton);
-    };
+    }
 
     getYourIncomeBeforeTax() {
         return this.driver.getValue(this.incomeBeforeTax);
-    };
+    }
 
     getOtherIncome() {
         return this.driver.getValue(this.otherIncome);
-    };
+    }
 
     getLivingExpenses() {
         return this.driver.getValue(this.livingExpenses);
-    };
+    }
 
     getOtherLoanRepayments() {
         return this.driver.getValue(this.otherLoanRepayments);
-    };
+    }
 
     getTotalCreditCardLimits() {
         return this.driver.getValue(this.totalCreditCardLimits);
-    };
-};
+    }
+}
 
 module.exports = HowMuchCouldIBorrow;
